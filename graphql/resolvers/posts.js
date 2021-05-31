d1c6f920fa1b9e54c92d3e14fc155710af70d13b
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-31 10:59:16 +0530

    Added the likePost functionallity

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
 
@@ -32,6 +32,10 @@ module.exports = {
       const user = checkAuth(context);
       console.log(user);
 
+      if (body.trim() === "") {
+        throw new Error("Post must not be empty");
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
@@ -56,6 +60,27 @@ module.exports = {
         }
       } catch (err) {
         throw new Error(err);
+      }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already liked!, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          //Not liked!, like it
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post Not Found!");
       }
     },
   },
