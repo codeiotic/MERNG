ec49c6c785f917af0a35ec3382987581addf9f41
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-31 12:22:53 +0530

    Created the 3 routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <div>
-      <h1>Hi, World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
