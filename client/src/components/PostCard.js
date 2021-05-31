04a48a8f26678968681588821443bdac2c18b6b1
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-31 14:15:58 +0530

    Created the PostCard Component

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,67 @@
+import { Card, Icon, Label, Button, Image } from "semantic-ui-react";
+import moment from "moment";
+import { Link } from "react-router-dom";
+
+const PostCard = ({
+  post: {
+    id,
+    username,
+    body,
+    createdAt,
+    comments,
+    commentCount,
+    likeCount,
+    likes,
+  },
+}) => {
+  const likePost = () => {
+    console.log("Like Post!!!");
+  };
+
+  const commentOnPost = () => {
+    console.log("Comment on Post!!!");
+  };
+
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+        ></Image>
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/post/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button as="div" labelPosition="right" onClick={likePost}>
+          <Button color="teal" basic>
+            <Icon
+              name="heart outline"
+              style={{ color: "teal", fontSize: "16px" }}
+            />
+          </Button>
+          <Label basic color="teal" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
+        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+          <Button color="blue" basic>
+            <Icon
+              name="comments outline"
+              style={{ color: "teal", fontSize: "16px" }}
+            />
+          </Button>
+          <Label basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default PostCard;
