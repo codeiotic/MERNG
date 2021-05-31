4d7ff7b3c1a6e41bc9de41fed764f19c6b3c9a9b
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-05-31 14:16:18 +0530

    Created the Home Page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,57 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
+
 const Home = () => {
+  const {
+    loading,
+    data: { getPosts: posts },
+  } = useQuery(FETCH_POSTS_QUERY);
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading Posts...</h1>
+        ) : (
+          posts &&
+          posts.map((post, index) => (
+            <Grid.Column key={index} style={{ marginBottom: "20px" }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   );
 };
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      comments {
+        id
+        createdAt
+        username
+        body
+      }
+      commentCount
+      likes {
+        id
+        createdAt
+        username
+      }
+      likeCount
+    }
+  }
+`;
 
 export default Home;
