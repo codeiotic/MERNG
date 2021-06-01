6c5c282dcb8df6b8c75ba99d70a8dace0f29dc6f
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-06-01 13:07:07 +0530

    Added react-skeleton for loading

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,8 @@
 import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
-import { Grid } from "semantic-ui-react";
+import { Grid, Card } from "semantic-ui-react";
 import PostCard from "../components/PostCard";
-
-const Home = () => {
-  const {
-    loading,
-    data: { getPosts: posts },
-  } = useQuery(FETCH_POSTS_QUERY);
-
-  return (
-    <Grid columns={3}>
-      <Grid.Row>
-        <h1>Recent Posts</h1>
-      </Grid.Row>
-      <Grid.Row>
-        {loading ? (
-          <h1>Loading Posts...</h1>
-        ) : (
-          posts &&
-          posts.map((post, index) => (
-            <Grid.Column key={index} style={{ marginBottom: "20px" }}>
-              <PostCard post={post} />
-            </Grid.Column>
-          ))
-        )}
-      </Grid.Row>
-    </Grid>
-  );
-};
+import Skeleton from "react-loading-skeleton";
 
 const FETCH_POSTS_QUERY = gql`
   {
@@ -53,5 +27,75 @@ const FETCH_POSTS_QUERY = gql`
     }
   }
 `;
+
+const Home = () => {
+  const { loading, error, data } = useQuery(FETCH_POSTS_QUERY);
+
+  if (error) {
+    return (
+      <h1 className="page-title">An Error Occurred! Please try again later</h1>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <Grid columns="three" divided>
+          <Grid.Row className="page-title">
+            <h1>Loading....</h1>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Card fluid>
+                <Card.Content>
+                  <Skeleton count={4} />
+                </Card.Content>
+                <Card.Content extra>
+                  <Skeleton count={1} />
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column>
+              <Card fluid>
+                <Card.Content>
+                  <Skeleton count={4} />
+                </Card.Content>
+                <Card.Content extra>
+                  <Skeleton count={1} />
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column>
+              <Card fluid>
+                <Card.Content>
+                  <Skeleton count={4} />
+                </Card.Content>
+                <Card.Content extra>
+                  <Skeleton count={1} />
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
+  } else {
+    return (
+      <Grid columns={3} divided>
+        <Grid.Row className="page-title">
+          <h1>Recent Posts</h1>
+        </Grid.Row>
+        <Grid.Row>
+          {data.getPosts &&
+            data.getPosts.map((post, index) => (
+              <Grid.Column key={index} style={{ marginBottom: "20px" }}>
+                <PostCard post={post} />
+              </Grid.Column>
+            ))}
+        </Grid.Row>
+      </Grid>
+    );
+  }
+};
 
 export default Home;
