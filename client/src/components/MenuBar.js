51efd04192b3417f209303374e4a1a5056980a30
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-06-01 20:10:30 +0530

    Created an optional chaining for checking user

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import { useState, useContext } from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth";
 
 const MenuBar = () => {
   const pathName = window.location.pathname; // returns '/', '/login' or '/register'
   const path = pathName === "/" ? "home" : pathName.substr(1);
+
   const [activeItem, setActiveItem] = useState(path);
+  const { user, logout } = useContext(AuthContext);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-  return (
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="blue">
+      <Menu.Item name={user.username} active as={Link} to="/" />
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size="massive" color="blue">
       <Menu.Item
         name="home"
@@ -36,6 +46,8 @@ const MenuBar = () => {
       </Menu.Menu>
     </Menu>
   );
+
+  return menuBar;
 };
 
 export default MenuBar;
