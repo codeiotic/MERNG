b9472c7d968276dc8bb0acfc572a78f3e7006f3c
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-06-01 20:09:59 +0530

    Used the AuthRoute's

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,21 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import MenuBar from "./components/MenuBar";
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/AuthRoute";
 
 function App() {
   return (
-    <Router>
-      <Container>
-        <MenuBar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
