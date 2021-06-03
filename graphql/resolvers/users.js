72d02c7114a25ba2828429c4f4c025894acea2b6
ProProgrammer2504 <73734376+ProProgrammer2504@users.noreply.github.com>
2021-06-03 14:28:28 +0530

    Fixed formatting

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,13 +1,13 @@
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-
-const User = require("../../models/User");
-const { SECRET_KEY } = require("../../config");
+const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
+
 const {
   validateRegisterInput,
   validateLoginInput,
 } = require("../../util/validators");
+const { SECRET_KEY } = require("../../config");
+const User = require("../../models/User");
 
 function generateToken(user) {
   return jwt.sign(
@@ -42,6 +42,7 @@ module.exports = {
         errors.general = "User not found!";
         throw new UserInputError("User not found!", { errors });
       }
+
       const token = generateToken(user);
 
       return {
@@ -54,10 +55,7 @@ module.exports = {
       _,
       { registerInput: { username, email, password, confirmPassword } }
     ) {
-      //   ! Validate User Data! - Done
-      //   ! Make sure User doesn't already exist - Done.
-      //   ! Hash the Password and create an auth token - Done
-
+      // Validate user data
       const { valid, errors } = validateRegisterInput(
         username,
         email,
@@ -67,7 +65,7 @@ module.exports = {
       if (!valid) {
         throw new UserInputError("Errors", { errors });
       }
-
+      // TODO: Make sure user doesn't already exist
       const user = await User.findOne({ username });
       if (user) {
         throw new UserInputError("Username is taken", {
@@ -76,7 +74,7 @@ module.exports = {
           },
         });
       }
-
+      // hash password and create an auth token
       password = await bcrypt.hash(password, 12);
 
       const newUser = new User({
